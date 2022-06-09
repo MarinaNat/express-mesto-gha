befd7d67cf3479efae91d61fb4d3e882b45444a8
Marina Natalina <Kiska21mk@yandex.ru>
2022-06-09 18:52:28 +0300

    13ПР

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(express.json());
+
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
@@ -20,7 +22,16 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/', (req, res) => {
+  res.send('<h1>Домашняя страница</h1>');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', userRouter);
 app.use('/', cardRouter);
 
