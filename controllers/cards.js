913c45cab4a8e740c21b7a829cbac8d162ec7707
Marina Natalina <Kiska21mk@yandex.ru>
2022-06-09 23:24:08 +0300

    13ПР исправила controllers cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -7,79 +7,93 @@ const {
 } = require('../utils/error');
 
 // Запрос всех карточек
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (_req, res) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(ERROR_DEFOULT).send({ message: err.message }));
+    .populate('owner')
+    .then((card) => res.send(card))
+    .catch(() => res.status(ERROR_DEFOULT).send({ message: 'Ошибка на сервере' }));
 };
 
 // Создание карточки
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  const { _id } = req.user;
-  Card.create({ name, link, owner: _id })
-    .then((card) => res.send({ data: card }))
+  // const { _id } = req.user;
+  const owner = req.user._id;
+  Card.create({ name, link, owner })
+    .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(ERROR_CODE).send({ message: err.message });
+        return res.status(ERROR_CODE).send({ message: 'Некорректные данные' });
       }
-      return res.status(ERROR_DEFOULT).send({ message: err.message });
+      return res.status(ERROR_DEFOULT).send({ message: 'Ошибка сервера' });
     });
 };
 
 // Удаление карточки
 module.exports.deleteCard = (req, res) => {
-  const { _id } = req.user;
-  Card.findOneAndDelete(_id)
+  // const { _id } = req.user;
+  Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
       if (!card) {
-        return res.status(NOT_FOUND).send({ message: 'Картока не найдена' });
+        res.status(NOT_FOUND).send({ message: 'Картока не найдена' });
+        return;
       }
-      return res.send({ data: card });
+      res.send(card);
     })
-    .catch((err) => res.status(ERROR_DEFOULT).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({ message: 'Некорректные данные' });
+      }
+      return res.status(ERROR_DEFOULT).send({ message: 'Ошибка сервера' });
+    });
 };
 
 // Добавления лайка
 module.exports.likeCard = (req, res) => {
-  const { _id } = req.user;
-  const { cardId } = req.params;
+  // const { _id } = req.user;
+  // const { cardId } = req.params;
   Card.findByIdAndUpdate(
-    cardId,
+    req.params.cardId,
     // добавить _id в массив, если его там нет
-    { $addToSet: { likes: _id } },
-    { new: true, runValidators: true },
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
   )
     .then((card) => {
       if (!card) {
-        return res.status(NOT_FOUND).send({ message: 'Картока не найдена' });
+        res.status(NOT_FOUND).send({ message: 'Картока не найдена' });
+        return;
       }
-      return res.send({ data: card });
+      res.send(card);
     })
     .catch((err) => {
-      if (err.name === 'SomeErrorName') {
-        return res.status(ERROR_CODE).send({ message: err.message });
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({ message: 'Некорректные данные' });
       }
-      return res.status(ERROR_DEFOULT).send({ message: err.message });
+      return res.status(ERROR_DEFOULT).send({ message: 'Ошибка сервера' });
     });
 };
 
 // Снятие лайка с карточки
 module.exports.dislikeCard = (req, res) => {
-  const { _id } = req.user;
-  const { cardId } = req.params;
+  // const { _id } = req.user;
+  // const { cardId } = req.params;
   // убрать _id из массива
-  Card.findOneAndUpdate(cardId, { $pull: { likes: _id } }, { new: true, runValidators: true })
+  Card.findOneAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
     .then((card) => {
       if (!card) {
-        return res.status(NOT_FOUND).send({ message: 'Картока не найдена' });
+        res.status(NOT_FOUND).send({ message: 'Картока не найдена' });
+        return;
       }
-      return res.send({ data: card });
+      res.send(card);
     })
     .catch((err) => {
-      if (err.name === 'SomeErrorName') {
-        return res.status(ERROR_CODE).send({ message: err.message });
+      if (err.name === 'CastError') {
+        return res.status(ERROR_CODE).send({ message: 'Некорректные данные' });
       }
-      return res.status(ERROR_DEFOULT).send({ message: err.message });
+      return res.status(ERROR_DEFOULT).send({ message: 'Ошибка сервера' });
     });
 };
