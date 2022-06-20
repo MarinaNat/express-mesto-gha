8ed6a768395b43b573979363e2cd6269d0c722e0
Marina Natalina <Kiska21mk@yandex.ru>
2022-06-20 16:32:48 +0300

    14ПР

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '62a1dac70b3144cc6feef0d0',
-    // вставьте сюда _id созданного в предыдущем пункте пользователя
-  };
-  next();
-});
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: '62a1dac70b3144cc6feef0d0',
+//     // вставьте сюда _id созданного в предыдущем пункте пользователя
+//   };
+//   next();
+// });
 app.post(
   '/signin',
   celebrate({
